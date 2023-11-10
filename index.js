b5c13505c25df420be729261d1ff40a7be69d24f
hienvu <mienvtph30480@fpt.edu.vn>
2023-11-10 08:39:53 +0700

    update code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+console.log("hellohhhhhhhhhhhhhhhh");
+var test = function (a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+};
+test(1, 3);
+var names = ['name1', 'name2', 'name3'];
+names.push('name4');
+console.log(names[0]);
--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-console.log("hello");
+console.log("hellohhhhhhhhhhhhhhhh");
 
-
-type teacher = {
-    name: string,
-    age:number,
+const test = (a:number, b:number ,c?: number | string) => {
+    console.log(a);
+    console.log(b);
+    console.log(c);
     
 }
+test(1,3);
+
+let names = ['name1' , 'name2' , 'name3'];
+names.push('name4');
+
+console.log(names[0]);
